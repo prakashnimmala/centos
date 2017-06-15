31d672d6eab76eb59f902d0ce434b9219f16b620
prakashnimmala <prakashn@snp.com>
2017-06-16 00:05:04 +0530

    Create mongo-install-centos.sh

--- mongo-install-centos.sh
+++ mongo-install-centos.sh
@@ -1,26 +1,23 @@
 # Install updates
 sudo yum -y update
 
-install_ap() {
+
+#install apache 2.4 php5
+sudo yum install httpd php php-mysql -y
 
 
-	#install apache 2.4 php5
-	sudo yum install httpd php php-mysql -y
+#start httpd
+sudo service httpd start
 
+#auto-start 
+sudo chkconfig httpd on
+sudo chkconfig firewalld off
+sudo chkconfig iptables off
+sudo service firewalld stop
+sudo service iptables stop
 
-	#start httpd
-	sudo service httpd start
+#set selinux
+sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
+sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+sudo setenforce 0
 
-	#auto-start 
-	sudo chkconfig httpd on
-	sudo chkconfig firewalld off
-	sudo chkconfig iptables off
-	sudo service firewalld stop
-	sudo service iptables stop
-
-	#set selinux
-	sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
-	sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
-	sudo setenforce 0
-
-}
